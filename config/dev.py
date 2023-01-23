bc8cdab4cf05b684307860927e35052d853c0bd3
Ashish <ashish.patil401@gmail.com>
2023-01-23 21:20:45 +0530

    updated config

--- config/dev.py
+++ config/dev.py
@@ -3,6 +3,6 @@ import os
 
 DEBUG = True
 SECRET_KEY = os.environ.get('DB_SECRET_KEY')
-SQLALCHEMY_DATABASE_URI= f"postgresql://postgres:{SECRET_KEY}@localhost:5432/bookstore",
+SQLALCHEMY_DATABASE_URI= f"postgresql://postgres:{SECRET_KEY}@localhost:5432/bookstore"
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
