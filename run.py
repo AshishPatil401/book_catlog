f0d176bb087c460e354f8f28df7f96569d193dda
Ashish <ashish.patil401@gmail.com>
2023-01-27 17:28:38 +0530

    updated run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+from app.auth.models import User
 
 
 if __name__ == '__main__':
@@ -6,6 +7,11 @@ if __name__ == '__main__':
     
     with flask_app.app_context():
         db.create_all()
+
+        if not User.query.filter_by(user_name="harry").first():
+            User.create_user(user="harry",
+                             email="harry@example.com",
+                             password="password")
 
     flask_app.run()
 
