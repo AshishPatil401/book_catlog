41db81aee2c9a98ef0e42818e60856fe77c3fadd
Ashish <ashish.patil401@gmail.com>
2023-01-30 22:19:17 +0530

    updated auth package

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,14 +1,14 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, validators
-from wtforms.validators import DataRequired, email, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
 
 class RegistrationForm(FlaskForm):
 
-    name = StringField('Name')
-    email = StringField('Email', validators=[DataRequired(), email()])
-    password = PasswordField('Password')
+    name = StringField('Name', validators=[DataRequired(), Length(2,20, message="Name should be at least 2 characters and less than 20 characters")])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(5), EqualTo('confirm', 'Password must match.')])
     confirm = PasswordField('Confirm')
     submit = SubmitField('Register')
 
